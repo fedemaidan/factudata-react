71c790998546f08b0c3ef2c20cc11df7009517f9
Federico Maidan <fede.maidan@gmail.com>
2026-01-09 13:47:46 -0300

    permisos odoo

--- src/sections/empresa/configuracionGeneral.js
+++ src/sections/empresa/configuracionGeneral.js
@@ -202,8 +202,8 @@ export const ConfiguracionGeneral = ({ empresa, updateEmpresaData, hasPermission
     "CREAR_SOLICITUD_MATERIAL",
     "VER_STOCK_MOVIMIENTOS",
     "VER_INVENTARIO_PRODUCTOS",
-    "GUARDAR_CONFIRMANDO",
-    "FACTURAS_PROVEEDOR_NO_EDI",
+    "ODOO_GUARDAR_CONFIRMANDO",
+    "ODOO_FACTURAS_PROVEEDOR_NO_EDI",
   ];
 
   const celulandiaAcciones = [
